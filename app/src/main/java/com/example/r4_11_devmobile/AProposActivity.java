4fc170d7ca07e59b06b34c897e44b900e32efef7
Inekô <115212177+Inepo@users.noreply.github.com>
2024-03-13 16:53:35 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/r4_11_devmobile/AProposActivity.java
@@ -0,0 +1,14 @@
+package com.example.r4_11_devmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AProposActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apropos);
+    }
+}
